29ba7a6c5548b94b0ea9d717694ec12026e679a8
MrKeith99 <kvalentincardenas@hotmail.com>
2024-08-29 12:02:51 +0900

    [ADD] installer

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "╔══╣ Install: BBox to TF (STARTING) ╠══╗"
+
+
+# Keep track of the current directory
+DIR=`pwd`
+
+
+# Clone required packages
+cd ..
+git clone -b feature/humble-devel https://github.com/TeamSOBITS/sobits_msgs.git
+
+# Install common dependencies
+python3 -m pip install -U pip
+python3 -m pip install \
+    matplotlib \
+
+sudo apt-get update
+sudo apt-get install -y \
+    ros-$ROS_DISTRO-tf2 \
+    ros-$ROS_DISTRO-tf2-ros \
+    ros-$ROS_DISTRO-geometry-msgs \
+    ros-$ROS_DISTRO-sensor-msgs \
+    ros-$ROS_DISTRO-pcl-ros \
+    ros-$ROS_DISTRO-pcl-conversions \
+    ros-$ROS_DISTRO-pcl-msgs \
+    ros-$ROS_DISTRO-message-filters
+
+
+# Go back to previous directory
+cd ${DIR}
+
+
+echo "╚══╣ Install: BBox to TF (FINISHED) ╠══╝"
