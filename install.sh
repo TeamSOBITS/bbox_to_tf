42e0290d700233935275529bebd5aae7e6c23cdd
MrKeith99 <kvalentincardenas@hotmail.com>
2025-11-05 22:05:23 +0900

    [UPD] jazzy support and distro selection

--- install.sh
+++ install.sh
@@ -9,25 +9,24 @@ DIR=`pwd`
 
 # Clone required packages
 cd ..
-git clone -b feature/humble-devel https://github.com/TeamSOBITS/sobits_interfaces.git
+git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/sobits_interfaces.git
 
 # Install common dependencies
-python3 -m pip install -U pip
-python3 -m pip install \
-    matplotlib \
+python3 -m pip install --break-system-packages -U pip
+python3 -m pip install --break-system-packages\
+    matplotlib 
 
 sudo apt update
 sudo apt install -y \
-    ros-$ROS_DISTRO-vision-msgs \
-    ros-$ROS_DISTRO-tf2 \
-    ros-$ROS_DISTRO-tf2-ros \
-    ros-$ROS_DISTRO-geometry-msgs \
-    ros-$ROS_DISTRO-sensor-msgs \
-    ros-$ROS_DISTRO-pcl-ros \
-    ros-$ROS_DISTRO-pcl-conversions \
-    ros-$ROS_DISTRO-pcl-msgs \
-    ros-$ROS_DISTRO-message-filters
-
+    ros-${ROS_DISTRO}-rclcpp \
+    ros-${ROS_DISTRO}-pcl-conversions \
+    ros-${ROS_DISTRO}-pcl-ros \
+    ros-${ROS_DISTRO}-tf2-ros \
+    ros-${ROS_DISTRO}-vision-msgs \
+    ros-${ROS_DISTRO}-sensor-msgs \
+    ros-${ROS_DISTRO}-geometry-msgs \
+    ros-${ROS_DISTRO}-std-msgs \
+    ros-${ROS_DISTRO}-std-srvs
 
 # Go back to previous directory
 cd ${DIR}
