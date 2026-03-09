a85186c201e82994084203f441c70cf36daa20c5
MrKeith99 <kvalentincardenas@hotmail.com>
2026-03-09 21:35:39 +0900

    [UPD] launch nodes with composable nodes

--- launch/keypoint_to_3d.launch.py
+++ launch/keypoint_to_3d.launch.py
@@ -2,12 +2,10 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import LifecycleNode
-from launch_ros.events.lifecycle import ChangeState
-from launch_ros.event_handlers import OnStateTransition
-import lifecycle_msgs.msg
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
     pkg_dir = get_package_share_directory('image_to_position')
@@ -20,16 +18,33 @@ def generate_launch_description():
         description='Full path to the ROS2 parameters file to use'
     )
 
-    keypoint_node = LifecycleNode(
-        package='image_to_position',
-        executable='keypoint_to_3d',
-        name='keypoint_to_3d',
-        namespace='',
-        parameters=[params_file],
-        output='screen'
+    namespace = LaunchConfiguration("namespace")
+    namespace_cmd = DeclareLaunchArgument(
+        "namespace",
+        description="Namespace for the nodes (String)",
+        default_value="",
+    )
+
+    container = ComposableNodeContainer(
+        name='keypoint_container',
+        namespace=namespace,
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='image_to_position',
+                plugin='image_to_position::KeypointTo3D',
+                name='keypoint_to_3d',
+                namespace=namespace,
+                parameters=[params_file],
+                extra_arguments=[{'use_intra_process_comms': True}]
+            ),
+        ],
+        output='screen',
     )
 
     return LaunchDescription([
         params_file_arg,
-        keypoint_node,
+        namespace_cmd,
+        container,
     ])
